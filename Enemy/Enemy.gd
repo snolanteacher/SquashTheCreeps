38b42de6759efb8871d2cbfa86797fcef597365b
gamecoder-nz <42922238+gamecoder-nz@users.noreply.github.com>
2022-11-22 12:04:03 +1300

    Created initial version

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -7,7 +7,6 @@ extends KinematicBody2D
 var velocity = Vector2()
 export var gravity = 3000
 export var move_speed = 300
-export var jump_speed = 1000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,23 +18,15 @@ func _ready():
 #	pass
 
 func _physics_process(delta):
-	velocity.x = 0
-	if Input.is_action_pressed("left"):
-		velocity.x -= move_speed
-	if Input.is_action_pressed("right"):
-		velocity.x += move_speed
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = -jump_speed
+	velocity.x = move_speed
 		
 	velocity.y += gravity * delta
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
-
-
-func _on_EnemyDetector_body_entered(body):
-	queue_free()
-	get_tree().change_scene("res://User Interface/GameOver.tscn")
+	
+	if is_on_wall():
+		move_speed = -move_speed
 
 
 func _on_StompDetector_body_entered(body):
-	velocity.y = -jump_speed
+	queue_free()
